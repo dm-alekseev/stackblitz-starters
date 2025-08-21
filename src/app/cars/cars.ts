fa35e55d833c98edd47304c9e5e9811ad51498da
dm-alekseev <alekseev@stw.by>
2025-08-21 16:52:57 +0300

    modified:   src/app/cars/cars.ts

--- src/app/cars/cars.ts
+++ src/app/cars/cars.ts
@@ -34,6 +34,54 @@ export class Cars {
       text: 'Мечта на колесах. Ferrari F8 Spider: 720 л.с., аэродинамика F1 и открытая кабина для тех, кто живет на полной скорости.',
       prices: [1450, 1300, 1100],
     },
+    {
+      images: 'https://testologia.ru/cars-images/5.png',
+      title: 'Porsche 911 Targa 4S Yellow',
+      text: 'Мечта на колесах. Ferrari F8 Spider: 720 л.с., аэродинамика F1 и открытая кабина для тех, кто живет на полной скорости.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/6.png',
+      title: 'Mercedes SL 55 AMG',
+      text: ' Классика спортивного шика. Mercedes SL 55 AMG: роскошь, кабриолет и мощь 469 л.с. для незабываемых поездок.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/7.png',
+      title: 'BMW Z4',
+      text: 'Компактный кабриолет для стильных путешествий. BMW Z4: идеальный союз маневренности, мощности и элегантного дизайна.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/8.png',
+      title: 'Mercedes C180 Convertible',
+      text: 'Идеальный выбор для солнечного дня. Mercedes C180 Convertible: кабриолет для легкого и комфортного вождения.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/9.png',
+      title: 'Chevrolet Corvette Orange',
+      text: 'Яркий, мощный, незабываемый. Chevrolet Corvette в оранжевом цвете: 495 л.с. и стиль, который говорит сам за себя.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/10.png',
+      title: 'BMW Z4',
+      text: 'Мечта на колесах. Ferrari F8 Spider: 720 л.с., аэродинамика F1 и открытая кабина для тех, кто живет на полной скорости.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/11.png',
+      title: 'Mercedes C180 Convertible',
+      text: 'Мечта на колесах. Ferrari F8 Spider: 720 л.с., аэродинамика F1 и открытая кабина для тех, кто живет на полной скорости.',
+      prices: [1450, 1300, 1100],
+    },
+    {
+      images: 'https://testologia.ru/cars-images/12.png',
+      title: 'Mercedes C180 Convertible',
+      text: 'Мечта на колесах. Ferrari F8 Spider: 720 л.с., аэродинамика F1 и открытая кабина для тех, кто живет на полной скорости.',
+      prices: [1450, 1300, 1100],
+    },
   ];
 
   carsFilter = [
@@ -42,16 +90,36 @@ export class Cars {
       name: 'Все марки',
     },
     {
-      active: true,
+      active: false,
       name: 'BMW',
     },
     {
-      active: true,
+      active: false,
       name: 'Lamborghini',
     },
     {
-      active: true,
+      active: false,
       name: 'Ferrari',
+    },
+    {
+      active: false,
+      name: 'Porsche',
+    },
+    {
+      active: false,
+      name: 'Mercedes',
+    },
+    {
+      active: false,
+      name: 'Chevrolet',
+    },
+    {
+      active: false,
+      name: 'Ford',
+    },
+    {
+      active: false,
+      name: 'Audi',
     },
   ];
 
@@ -69,7 +137,7 @@ export class Cars {
     this.carsFilter.forEach((el) => (el.active = false));
     filter.active = true;
 
-    const filterText = filter.name.toLowwCase();
+    const filterText = filter.name.toLowerCase();
 
     if (filterText === 'все марки') {
       this.cars = -this.baseCars;
